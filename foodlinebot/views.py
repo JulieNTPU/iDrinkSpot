0861555c07bc55b5fed4c430ff6c29a0592f5f9e
Yi-Jing Chen <aileechen0408@gmail.com>
2023-05-24 17:17:25 +0800

    新增CarouselTemplate選單，最多10筆資料

--- foodlinebot/views.py
+++ foodlinebot/views.py
@@ -10,7 +10,12 @@ from linebot.models import (
     TextSendMessage,
     TemplateSendMessage,
     ButtonsTemplate,
-    MessageTemplateAction
+    MessageTemplateAction,
+    CarouselTemplate,
+    CarouselColumn,
+    MessageAction,
+    PostbackAction,
+    URIAction
 )
 
 
@@ -39,7 +44,9 @@ def callback(request):
  
         for event in events:
             if isinstance(event, MessageEvent):  # 如果有訊息事件
+
                 '''
+                #linebot 只回傳文字
                 food = IFoodie(event.message.text)  #使用者傳入的訊息文字
                 line_bot_api.reply_message(  # 回復訊息
                     event.reply_token,
@@ -47,7 +54,8 @@ def callback(request):
                     TextSendMessage(text=food.scrape()) # 回應前五間最高人氣且營業中的餐廳訊息文字
                 )
                 '''
-
+                '''
+                #linebot 回傳按鈕，最多四個
                 if event.message.text == "哈囉":
                      line_bot_api.reply_message(  # 回復傳入的訊息文字
                         event.reply_token,
@@ -84,6 +92,117 @@ def callback(request):
                         event.reply_token,
                         TextSendMessage(text=food.scrape())
                     )
+                '''
+                
+                #linebot 回傳 CarouselTemplate 一次最多十個
+                if event.message.text == "哈囉":
+                    line_bot_api.reply_message(  # 回復傳入的訊息文字
+                        event.reply_token,
+                        TemplateSendMessage(
+                            alt_text='CarouselTemplate', 
+                            template=CarouselTemplate(
+                                columns=[
+                                    CarouselColumn(
+                                        #thumbnail_image_url='https://scontent-tpe1-1.xx.fbcdn.net/v/t39.30808-6/270016692_916592362328666_8079011016379207964_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=noyydldOoz4AX8a_ciM&_nc_ht=scontent-tpe1-1.xx&oh=00_AfA91FCOMU6QSrUfaT8VuJD23OETOykHfgpmvVjkFDWeRA&oe=6472AF3B',
+                                        title='選單 1',
+                                        text='說明文字 1',
+                                        actions=[
+                                            PostbackAction(
+                                            label='postback',
+                                            data='data1'
+                                            ),
+                                            MessageAction(
+                                                label='哈囉',
+                                                text='哈囉'
+                                            ),
+                                            URIAction(
+                                                label='youtube url',
+                                                uri='https://www.youtube.com/'
+                                            )
+                                        ]
+                                    ),
+                                    CarouselColumn(
+                                        #thumbnail_image_url='hhttps://steam.oxxostudio.tw/download/python/line-template-message-demo2.jpg',
+                                        title='選單 2',
+                                        text='說明文字 2',
+                                        actions=[
+                                            PostbackAction(
+                                            label='postback',
+                                            data='data1'
+                                            ),
+                                            MessageAction(
+                                                label='台北市',
+                                                text='台北市'
+                                            ),
+                                            URIAction(
+                                                label='茶湯會',
+                                                uri='https://tw.tp-tea.com/'
+                                            )
+                                        ]
+                                    ),
+                                    CarouselColumn(
+                                        #thumbnail_image_url='hhttps://steam.oxxostudio.tw/download/python/line-template-message-demo2.jpg',
+                                        title='選單 3',
+                                        text='說明文字 3',
+                                        actions=[
+                                            PostbackAction(
+                                            label='postback',
+                                            data='data1'
+                                            ),
+                                            MessageAction(
+                                                label='台北市',
+                                                text='台北市'
+                                            ),
+                                            URIAction(
+                                                label='茶湯會',
+                                                uri='https://tw.tp-tea.com/'
+                                            )
+                                        ]
+                                    ),
+                                    CarouselColumn(
+                                        #thumbnail_image_url='hhttps://steam.oxxostudio.tw/download/python/line-template-message-demo2.jpg',
+                                        title='選單 4',
+                                        text='說明文字 4',
+                                        actions=[
+                                            PostbackAction(
+                                            label='postback',
+                                            data='data1'
+                                            ),
+                                            MessageAction(
+                                                label='台北市',
+                                                text='台北市'
+                                            ),
+                                            URIAction(
+                                                label='茶湯會',
+                                                uri='https://tw.tp-tea.com/'
+                                            )
+                                        ]
+                                    ),
+                                    CarouselColumn(
+                                        #thumbnail_image_url='hhttps://steam.oxxostudio.tw/download/python/line-template-message-demo2.jpg',
+                                        title='選單 5',
+                                        text='說明文字 5',
+                                        actions=[
+                                            PostbackAction(
+                                            label='postback',
+                                            data='data1'
+                                            ),
+                                            MessageAction(
+                                                label='台北市',
+                                                text='台北市'
+                                            ),
+                                            URIAction(
+                                                label='茶湯會',
+                                                uri='https://tw.tp-tea.com/'
+                                            )
+                                        ]
+                                    )
+                                ]                        
+
+                            )
+                        )
+                    )
+
         return HttpResponse()
         
     else:
